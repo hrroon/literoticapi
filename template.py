f107c62d34e5970b15bae22758a00bcc0e9331c7
Jack White <NA>
2012-04-03 16:38:59 -0700

    this is what the beginnings of the files should look like

---
+++ template.py
@@ -0,0 +1,14 @@
+# This file is part of LiteroticaAPI.
+# 
+# LiteroticaAPI is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# LiteroticaAPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Lesser GNU General Public License for more details.
+# 
+# You should have received a copy of the Lesser GNU General Public License
+# along with LiteroticaAPI.  If not, see <http://www.gnu.org/licenses/>.
