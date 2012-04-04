1f4351ece008bb4da4892103e03e0a11f8de835d
Jack White <NA>
2012-04-03 21:51:04 -0700

    more setuptools stuff

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "LiteroticAPI",
-    version = "0.1.1"
+    # major.minor.bugfix
+    version = "0.1.1",
     packages = find_packages(),
 
     # reminder: add BeautifulSoup and requests
-    install_requires = ['docutils>=0.3'], 
+    install_requires = ['docutils>=0.3','BeautifulSoup==3','requests'], 
 
     author = "hrroon",
     description = "an API for Literotica",
