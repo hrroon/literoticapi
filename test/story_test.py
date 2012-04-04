2ac3459b896e8d161cc96d7360dac3942462f2b6
Jack White <NA>
2012-04-04 02:57:30 -0700

    more tests

--- test/story_test.py
+++ test/story_test.py
@@ -1,11 +1,29 @@
 import unittest
 
-import literoticapi
+from literoticapi.story import *
 
-class InitializeStoryTest(unittest.TestCase):
+class testStory(unittest.TestCase):
 
-    def test(self):
-        assert 1==1
+    def setUp(self):
+        self.story = Story('e-beth-ch-01')
+
+    def testGetAuthor(self):
+        assert self.story.get_author() == 'bluedragonauthor'
+
+    def testGetTitle(self):
+        assert self.story.get_title() == "E-Beth Ch. 01"
+
+    def testGetCategory(self):
+        assert self.story.get_category() == "Group Sex"
+
+    def testGetDescription(self):
+        assert self.story.get_description() == "E-Beth from 'The Book of David' searches for happiness."
+
+    def testGetNumPages(self):
+        assert self.story.get_num_pages() == 7
+
+    def testGetText(self):
+        print self.story.get_text()[-1]
 
 if __name__ == "__main__":
     unittest.main()
